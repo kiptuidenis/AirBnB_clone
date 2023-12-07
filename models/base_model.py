efa06982b274bd6a1622dfaa725468945d98b313
kiptuidenis <kiptuidenis@gmail.com>
2023-12-07 17:34:55 +0300

    Updates

--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,23 @@
 """This module contains the base class for the AirBnB project
 """
 from datetime import datetime
-import time
 import uuid
 
 
 class BaseModel:
     """Defines all common attributes and methods for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Class constructor
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            del kwargs['__class__']
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self.__dict__ = kwargs
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def save(self):
         """Updates the public instance attribute 'updated_at' 
