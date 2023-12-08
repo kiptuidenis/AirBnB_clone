58923653bfa13e75bcd215f022db66f44394b011
kiptuidenis <kiptuidenis@gmail.com>
2023-12-08 20:19:55 +0300

    Updates

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """This module contains the base class for the AirBnB project
 """
 from datetime import datetime
+from models import storage
 import uuid
 
 
@@ -19,12 +20,14 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         """Updates the public instance attribute 'updated_at' 
            with the current datetime
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """_summary_
