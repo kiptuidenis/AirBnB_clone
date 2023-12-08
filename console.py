86cad5e59242cbc7814d87e5a188db7aa78a2561
kiptuidenis <kiptuidenis@gmail.com>
2023-12-08 20:45:51 +0300

    Updates

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""This is the console"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """This class defines the console"""
+    prompt = "(hbnb)"
+
+    def do_quit(self, args):
+        """Quits the cmd interpreter"""
+        return True
+
+    def do_EOF(self, args):
+        """End of File"""
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
