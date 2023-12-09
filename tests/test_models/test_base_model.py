91ebe51594160236c1129c6d584b03e9f4f195ad
Denis Kiptui <115914094+kiptuidenis@users.noreply.github.com>
2023-12-09 15:35:50 +0300

    Added test to_dict() method

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -20,3 +20,15 @@ class TestBaseModel(unittest.TestCase):
         base_model.save()
         diff = base_model.updated_at - date_now
         self.assertTrue(abs(diff.total_seconds()) < 0.01)
+    def test_base_model_to_dict(self):
+        """Tests for to_dict()  on the base model class"""
+        base_model = BaseModel()
+        base_model.name = "Gabriel"
+        base_model.age = 30
+        user = base_model.to_dict()
+        self.assertEqual(user["id"], base_model.id)
+        self.assertEqual(user["__class__"], type(base_model).__name__)
+        self.assertEqual(user["created_at"], base_model.created_at.isoformat())
+        self.assertEqual(user["updated_at"], base_model.updated_at.isoformat())
+        self.assertEqual(user["name"], base_model.name)
+        self.assertEqual(user["age"], base_model.age)
