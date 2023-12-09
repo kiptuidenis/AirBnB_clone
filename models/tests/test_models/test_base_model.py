bf16ff4cbc4d5360d25c19da0aa3ffbe4e850fcf
brownskingirl <64357561+glohvall@users.noreply.github.com>
2023-12-09 11:53:09 +0000

    tets basemodels

---
+++ models/tests/test_models/test_base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    def test_BaseModel_init(self):
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+
+        self.assertEqual(my_model.number, 89)
