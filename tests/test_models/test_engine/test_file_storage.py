1a6e947cee0863bb7b91415f0732a61bf823e6d7
kiptuidenis <kiptuidenis@gmail.com>
2023-12-09 17:53:33 +0300

    Updates

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Tests file_storage"""
+import unittest
+from models.engine.file_storage import FileStorage
+
+class test_fileStorage(unittest.TestCase):
+    """Test FileStorage Class"""
+    def test_instances(self):
+        """Test for storage instance"""
+        obj = FileStorage()
+        self.assertIsInstance(obj, FileStorage)
