dfa3eec6d3d2844aca5f653c4d91855ade50e8ea
brownskingirl <64357561+glohvall@users.noreply.github.com>
2023-12-09 13:10:44 +0000

    test str

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -20,3 +20,16 @@ class TestBaseModel(unittest.TestCase):
         base_model.save()
         diff = base_model.updated_at - date_now
         self.assertTrue(abs(diff.total_seconds()) < 0.01)
+
+    def test_BaseModel_to_dict(self):
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        my_model_dict = my_model.to_dict()
+        self.assertEqual(my_model_dict["my_number"], 89)
+        self.assertEqual(my_model_dict["__class__"], "BaseModel")
+    
+    def test_BaseModel__str__(self):
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
