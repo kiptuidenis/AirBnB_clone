59991b665a74e545a5a672aac16a4a432f5ca63f
kiptuidenis <kiptuidenis@gmail.com>
2023-12-09 15:50:20 +0300

    Updates

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -16,23 +16,7 @@ class TestBaseModel(unittest.TestCase):
     def test_base_model_save(self):
         """Tests the save method on the base model class"""
         base_model = BaseModel()
-        date_now = datetime.now()
+        time_now = datetime.now()
         base_model.save()
-        diff = base_model.updated_at - date_now
-<<<<<<< HEAD
+        diff = base_model.updated_at - time_now
         self.assertTrue(abs(diff.total_seconds()) < 0.01)
-=======
-        self.assertTrue(abs(diff.total_seconds()) < 0.01)
-    def test_base_model_to_dict(self):
-        """Tests for to_dict()  on the base model class"""
-        base_model = BaseModel()
-        base_model.name = "Gabriel"
-        base_model.age = 30
-        user = base_model.to_dict()
-        self.assertEqual(user["id"], base_model.id)
-        self.assertEqual(user["__class__"], type(base_model).__name__)
-        self.assertEqual(user["created_at"], base_model.created_at.isoformat())
-        self.assertEqual(user["updated_at"], base_model.updated_at.isoformat())
-        self.assertEqual(user["name"], base_model.name)
-        self.assertEqual(user["age"], base_model.age)
->>>>>>> 91ebe51594160236c1129c6d584b03e9f4f195ad
