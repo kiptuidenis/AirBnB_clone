75344227c4ee664e33a972aa1436ee148fa7d977
kiptuidenis <kiptuidenis@gmail.com>
2023-12-09 15:17:08 +0300

    Test Cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,6 +2,9 @@
 import unittest
 from datetime import datetime
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+import os
+import json
 
 class TestBaseModel(unittest.TestCase):
     def test_BaseModel_init(self):
@@ -10,3 +13,16 @@ class TestBaseModel(unittest.TestCase):
         my_model.my_number = 89
 
         self.assertEqual(my_model.my_number, 89)
+
+    def test_base_model_instance_save(self):
+        """Tests that model save"""
+        base_model = BaseModel()
+        base_model.save()
+        key = "{}.{}".format(type(base_model).__name__, base_model.id)
+        obj = {key: base_model.to_dict()}
+        self.assertTrue(os.path.isfile(FileStorage._FileStorage__file_path))
+        with open(FileStorage._FileStorage__file_path,
+                  "r", encoding="utf-8") as f:
+            self.assertEqual(len(f.read()), len(json.dumps(obj)))
+            f.seek(0)
+            self.assertEqual(json.load(f), obj)
