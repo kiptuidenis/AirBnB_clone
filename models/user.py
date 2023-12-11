dd8f26860cefaa50c7dcfa4e13c1f9c5f24971d5
brownskingirl <64357561+glohvall@users.noreply.github.com>
2023-12-11 22:29:08 +0000

    user3

--- models/user.py
+++ models/user.py
@@ -4,9 +4,12 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    '''Initiallize User Class with public class attributes'''
+    '''Initialize User Class with public class attributes'''
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    def __init__(self, *args, **kwargs):
+        '''Initialize instance attributes'''
+        super().__init__(*args, **kwargs)  # Call the __init__ method of the base class
+        self.email = kwargs.get('email', "")
+        self.password = kwargs.get('password', "")
+        self.first_name = kwargs.get('first_name', "")
+        self.last_name = kwargs.get('last_name', "")
